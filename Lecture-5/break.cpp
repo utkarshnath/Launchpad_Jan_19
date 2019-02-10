6c5d7da9942f039fd3811f74f06aa6686bc4f270
utkarshnath <nath.utkarsh1@gmail.com>
2019-02-10 16:36:09 +0530

    5th lecture

--- Lecture-5/break.cpp
+++ Lecture-5/break.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
 
+    for(int i=0;i<10;i++){
+        cout<<"hello"<<endl;
+        if(i>3){
+            continue;
+        }
+        cout<<"hey!";
+    }
+    for(int i=0;i<10;i++){
+        if(i==3){
+            break;
+        }
+        for(int j=0;j<10;j++){
+            cout<<i<<" "<<j<<endl;
+        }
+        cout<<"HEY!"<<endl;
+    }
+}
--- Lecture-5/linearSearch.cpp
+++ Lecture-5/linearSearch.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 using namespace std;
+int global[100000000];
 int main(){
-int arr[10] = {1,2,3,44,56,7,0,8,9,10};
+int arr[1000000] = {1,2,3,44,56,7,0,8,9,10};
 int n;
 cin>>n;
 for(int i=0;i<10;i++){
